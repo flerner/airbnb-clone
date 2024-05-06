8bb0b13b65628656d0dde29c34a8bab73b1978ec
Felix Lerner <felix.lerner01@gmail.com>
2024-05-06 17:27:05 -0300

    modal and customized button

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={font.className}>
         {/* try deleting clientonly and check if it still works */}
         <ClientOnly>
-          <Modal />
+          <Modal isOpen title='Login Modal' />
           <Navbar />
         </ClientOnly>
 
