55035bc157c8426770c42a33551f6f28c6c46c3a
Felix Lerner <felix.lerner01@gmail.com>
2024-05-13 18:45:53 -0300

    feat: map leaflet

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,7 @@ import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import { FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../inputs/CountrySelect'
+import dynamic from 'next/dynamic'
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -41,6 +42,14 @@ const RentModal = () => {
   })
   const category = watch('category')
   const location = watch('location')
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  )
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
@@ -98,6 +107,8 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
+
+        <Map center={location?.latlng} />
       </div>
     )
   }
