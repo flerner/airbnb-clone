4724d6b954f059acc4a3ea95672ae50ff5563e58
Felix Lerner <felix.lerner01@gmail.com>
2024-05-16 18:08:27 -0300

    feat: display listing

---
+++ app/actions/getListings.ts
@@ -0,0 +1,12 @@
+export default async function getListings() {
+  try {
+    const listings = await prisma?.listing.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    return listings
+  } catch (err: any) {
+    throw new Error(err)
+  }
+}
