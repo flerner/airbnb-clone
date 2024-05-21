233e55b6aef84065673d95e6aec522b41d978e4d
Felix Lerner <felix.lerner01@gmail.com>
2024-05-21 18:43:02 -0300

    reservations cancel from creator of listing

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -59,7 +59,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label='My trips'
                 />
                 <MenuItem onClick={() => {}} label='My favourites' />
-                <MenuItem onClick={() => {}} label='My reservations' />
+                <MenuItem
+                  onClick={() => router.push('/reservations')}
+                  label='My reservations'
+                />
                 <MenuItem onClick={() => {}} label='My properties' />
                 <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
                 <MenuItem onClick={() => signOut()} label='Logout' />
